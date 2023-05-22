13e3f04ef8b212b365e25f0396422781e4aa054e
ys0222 <ggdbstjd0222@gmail.com>
2023-05-23 02:01:51 +0900

    [Feat] : Header Component 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+//내부
+import React from "react";
+
+//외부
+import styled from "styled-components";
+
+interface LoginStatus {
+    isLogin: boolean;
+  }
+
+export default function Header() {
+    const isLogin = true;
+    
+    return (
+        <Positioner>
+            <Logo>
+                📚                
+            </Logo>
+            
+            <Menu>
+                <LoginStatus isLogin={isLogin} />
+            </Menu>
+            
+        </Positioner>
+    )
+}
+
+//헤더 위치
+const Positioner = styled.div`
+
+  position : relative;
+  
+  padding : 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: black;
+
+`
+
+//로고
+const Logo = styled.div`
+    position : relative;
+    left : 5vw;
+`
+
+//로그인 / 로그아웃 상태표시
+const Menu = styled.div`
+    position : relative;
+    right : 5vw;
+`
+
+const LoginStatus: React.FC<LoginStatus> = ({ isLogin }) => {
+    return <div>{isLogin ? ("login") : ("Logout")}</div>;
+  };
