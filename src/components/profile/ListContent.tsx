e3e63f661c1283de4672df60e0e5e6774f197e59
ys0222 <ggdbstjd0222@gmail.com>
2023-10-25 02:15:33 +0900

    [Feat] : 프로필 도서 목록 없을때 안내 문구 구현

--- src/components/profile/ListContent.tsx
+++ src/components/profile/ListContent.tsx
@@ -1,138 +1,128 @@
-import { faStar } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { styled } from "styled-components"
-import truncate from "../../util/truncate"
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { styled } from "styled-components";
+import truncate from "../../util/truncate";
 
 interface DataType {
-    "id": number,
-    "title": string,
-    "score": string,
-    "price": number,
-    "coverImg": string,
-    "bookTitle": string,
-    "author": string,
-    "publisher": string,
-    "nickname": string,
-    "loginId": string,
-    "createdDate": string,
-    "view": number
+  "id": number,
+  "title": string,
+  "score": string,
+  "price": number,
+  "coverImg": string,
+  "bookTitle": string,
+  "author": string,
+  "publisher": string,
+  "nickname": string,
+  "loginId": string,
+  "createdDate": string,
+  "view": number
 }
 
 interface DataTypeList {
-    data: DataType[] | undefined;
+  data: DataType[] | undefined;
 }
 
+const Alert = styled.div`
+    display : flex;
+    justify-content : center;
+
+    padding : 50px;
+
+    
+`
+
 export default function ListContent({ data }: DataTypeList) {
-    return (
-        <>
-            {data?.map((book) => (
-                <>
-                    <ItemContainer >
-                        <Box>
+  if (!data || data.length === 0) {
+    return <Alert>해당하는 도서 목록이 없습니다 !</Alert>;
+  }
 
-                            <ItemImg src={book.coverImg} alt={book.title} />
-
-                            <Content>
-                                <ItemName>
-                                    <span>{book.title}</span>
-                                </ItemName>
-                                <BookTitle>
-                                    <span>{truncate(book.bookTitle, 27)}</span>
-                                </BookTitle>
-                                <ItemPublisher>
-                                    <span> {book.author.replace('^', ',')} / {book.publisher} </span>
-                                </ItemPublisher>
-                                <ItemRating>
-
-                                    {book.score ? 
-                                        <span>
-                                        <FontAwesomeIcon icon={faStar} className="star-icon" />
-                                        {book.score}
-                                        </span>
-                                        : <></>}
-                                    {book.price ?
-                                        <span> {book.price} ₩</span>
-                                        : <></>
-                                    }
-
-                                </ItemRating>
-                            </Content>
-                        </Box>
-                    </ItemContainer>
-                </>
-            ))}
-        </>
-    )
+  return (
+    <>
+      {data.map((book) => (
+        <div key={book.id}>
+          <ItemContainer>
+            <Box>
+              <ItemImg src={book.coverImg} alt={book.title} />
+              <Content>
+                <ItemName>
+                  <span>{book.title}</span>
+                </ItemName>
+                <BookTitle>
+                  <span>{truncate(book.bookTitle, 27)}</span>
+                </BookTitle>
+                <ItemPublisher>
+                  <span> {book.author.replace('^', ',')} / {book.publisher} </span>
+                </ItemPublisher>
+                <ItemRating>
+                  {book.score ? (
+                    <span>
+                      <FontAwesomeIcon icon={faStar} className="star-icon" />
+                      {book.score}
+                    </span>
+                  ) : null}
+                  {book.price ? <span> {book.price} ₩</span> : null}
+                </ItemRating>
+              </Content>
+            </Box>
+          </ItemContainer>
+        </div>
+      ))}
+    </>
+  );
 }
 
 const ItemContainer = styled.div`
-
-/* 상 우 하 좌 */
-margin : 0 15px 15px;
-height : 120px;
-
-`
+  /* 상 우 하 좌 */
+  margin: 0 15px 15px;
+  height: 120px;
+`;
 
 const Box = styled.div`
-    display: flex;
-
-    width : 100%;
-    height : 120px;
-    
-
-    background-color : #cacaca;
-    border-radius : 8px;
-    
-    &:hover {
-        background-color : #e2e2e2;
-        border-radius : 8px;
-
-        cursor : pointer;
-    }
-
-`
-
+  display: flex;
+  width: 100%;
+  height: 120px;
+  background-color: #cacaca;
+  border-radius: 8px;
+  &:hover {
+    background-color: #e2e2e2;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+`;
 
 const ItemImg = styled.img`
-
-padding : 7px;
-height :100px;
-
-`
+  padding: 7px;
+  height: 100px;
+`;
 
 const Content = styled.div`
-    display: flex;
-    flex-direction : column;
-    justify-content : flex-start;
-    padding : 0px 10px;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 0px 10px;
+`;
 
 const ItemName = styled.div`
+  span {
+    font-size: 20px;
+  }
+`;
 
-span{
-    font-size : 20px;
-}
-
-`
 const BookTitle = styled.div`
-span{
+  span {
     font-size: 16px;
-}
-`
+  }
+`;
 
 const ItemPublisher = styled.div`
-
-span {
-    font-size : 15px;
-    font-weight : 200;
-}
-`
+  span {
+    font-size: 15px;
+    font-weight: 200;
+  }
+`;
 
 const ItemRating = styled.div`
-
-span{
-    font-size : 15px;
-}
-
-
-`
+  span {
+    font-size: 15px;
+  }
+}`;
